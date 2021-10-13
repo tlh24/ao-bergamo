16e10fbe52b400cfbbf91fbb9dc4674e6d47369e
Tim Hanson <sideskate@gmail.com>
2021-10-13 18:45:01 -0400

    working on the NN DM controller .. not donegit status

--- ml/train_forward_transform.py
+++ ml/train_forward_transform.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import math
 from math import sqrt
 import pdb
+import pickle
 
 import torch
 
@@ -26,73 +27,69 @@ from torch.autograd import Function
 # from pympler.tracker import SummaryTracker
 
 class EqualLR:
-    def __init__(self, name):
-        self.name = name
+	def __init__(self, name):
+		self.name = name
 
-    def compute_weight(self, module):
-        weight = getattr(module, self.name + '_orig')
-        fan_in = weight.data.size(1) * weight.data[0][0].numel()
-        # print("compute_weight ", weight.size())
+	def compute_weight(self, module):
+		weight = getattr(module, self.name + '_orig')
+		fan_in = weight.data.size(1) * weight.data[0][0].numel()
+		# print("compute_weight ", weight.size(), 'fan_in', fan_in)
 
-        return weight * sqrt(2 / fan_in)
-    
-    # Idea: rather than changing the intialization of the weights, 
-    # here they just use N(0, 1), and dynamically scale the weights by 
-    # sqrt(2 / fan_in), per He 2015. 
-    # updates are applied to the unscaled weights (checked), 
-    # which means that the gradient updates are also scaled by sqrt(2/fan_in)
+		return weight * sqrt(2 / fan_in)
+	
+	# Idea: rather than changing the intialization of the weights, 
+	# here they just use N(0, 1), and dynamically scale the weights by 
+	# sqrt(2 / fan_in), per He 2015. 
+	# gradient updates are applied to the unscaled weights (checked), 
+	# which means that the gradient updates are also scaled by sqrt(2/fan_in)
 
-    @staticmethod
-    def apply(module, name):
-        fn = EqualLR(name)
+	@staticmethod
+	def apply(module, name):
+		fn = EqualLR(name)
 
-        weight = getattr(module, name)
-        del module._parameters[name]
-        module.register_parameter(name + '_orig', nn.Parameter(weight.data))
-        module.register_forward_pre_hook(fn)
+		weight = getattr(module, name)
+		del module._parameters[name]
+		module.register_parameter(name + '_orig', nn.Parameter(weight.data))
+		module.register_forward_pre_hook(fn)
 
-        return fn
+		return fn
 
-    def __call__(self, module, input):
-        weight = self.compute_weight(module)
-        setattr(module, self.name, weight)
+	def __call__(self, module, input):
+		weight = self.compute_weight(module)
+		setattr(module, self.name, weight)
 
 def equal_lr(module, name='weight'):
     EqualLR.apply(module, name)
-
     return module
 
 class EqualLinear(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super().__init__()
+	def __init__(self, in_dim, out_dim):
+		super().__init__()
 
-        linear = nn.Linear(in_dim, out_dim)
-        linear.weight.data.normal_()
-        linear.bias.data.zero_()
+		linear = nn.Linear(in_dim, out_dim)
+		linear.weight.data.normal_()
+		linear.bias.data.zero_()
 
-        self.linear = equal_lr(linear)
+		self.linear = equal_lr(linear)
 
-    def forward(self, input):
-        return self.linear(input)
+	def forward(self, input):
+		return self.linear(input)
+	  
+	def compute_weight(self):
+		w = self.linear.state_dict()['weight_orig']
+		fan_in = w.data.size(1) * w.data[0][0].numel() 
+		return w * (2 / sqrt(fan_in))
 
 device = torch.device('cuda:0')
 batch_size = 16 # 64 converges more slowly, obvi
 # batch_size = 32, after 50k steps validation loss is 0.000206
 # 16, validation loss is 0.000191
-niters = 500000
-
-f = h5py.File('../rundata/centroids_cleaned.mat', 'r')
-A = f.get('A')[:]
-v = f.get('v')[:]
-A = torch.from_numpy(A.astype(np.single)) 
-v = torch.from_numpy(v.astype(np.single))
-(nsamp, ncentroids) = A.shape;
-f.close()
-
 # layers: in, 1024, 512, 256, 97: validation loss 7e-5, 1.2e-4, 8.7e-5
 # layers: in, 1024, 1024 512, 256, 97: validation loss 1.3e-4
 # layers: in, 512, 256, 97: 1e-4
-
+# straight nn.Linear, no EqualLinear, 500k train steps before: 1.7e-3
+# -- EqualLinear really works! 
+ncentroids = 1075
 analyzer = nn.Sequential(
 	EqualLinear(ncentroids, 1024), 
 	nn.LeakyReLU(0.2), 
@@ -105,9 +102,20 @@ analyzer = nn.Sequential(
 optimizer = optim.AdamW(analyzer.parameters(), lr=0.001, betas=(0.0, 0.99), weight_decay=1e-3)
 lossfunc = torch.nn.SmoothL1Loss() # mean reduction
 
+niters = 5000
 slowloss = 0.0
 losses = np.zeros((2,niters)) 
 nvalidate = 50000
+
+f = h5py.File('../rundata/centroids_cleaned.mat', 'r')
+A = f.get('A')[:]
+v = f.get('v')[:]
+A = torch.from_numpy(A.astype(np.single)) 
+v = torch.from_numpy(v.astype(np.single))
+(nsamp, ncentroids) = A.shape;
+f.close()
+print(f'ncentroids:{ncentroids}'); 
+
 
 for k in range(niters):
 	r = torch.randint(nvalidate, nsamp, (batch_size,)) # on the cpu
@@ -134,21 +142,58 @@ with torch.no_grad():
 		x = A[r, :].cuda(device) # copy to gpu
 		y = v[r, :].cuda(device)
 		y = y / 0.15 # scale to +- 1.0
-		analyzer.zero_grad()
 		predict = analyzer(x)
 		sumloss = sumloss + lossfunc(y, predict)
 
 sumloss = sumloss / (nvalidate / batch_size)
 print(f'validation loss: {sumloss}')
-# save the results in a file! 
-#scipy.io.savemat('forward_nn.mat', mdict=
-                 #{'l1': analyzer.)
 
-#for name, param in analyzer.named_parameters():
-    #print('name: ', name)
-    #print(type(param))
-    #print('param.shape: ', param.shape)
-    #print('param.requires_grad: ', param.requires_grad)
-    #print('=====')
+# this muse be called after a gradient-free forward pass
+# which call the __call__ function of EqualLR class, 
+# thereby properly scaling the weights. 
+d = {}
+d['0.weight'] = analyzer[0].linear.weight
+d['0.bias'] = analyzer[0].linear.bias
+d['2.weight'] = analyzer[2].linear.weight
+d['2.bias'] = analyzer[2].linear.bias
+d['4.weight'] = analyzer[4].linear.weight
+d['4.bias'] = analyzer[4].linear.bias
+d['6.weight'] = analyzer[6].linear.weight
+d['6.bias'] = analyzer[6].linear.bias
 
-# wfile = h5py.File('../data/Cforward_nn.mat', analyzer.state_dict)
+for key in d:
+	val = d[key].cpu().detach()
+	np.save('dmcontrolnet/' + key + '.npy', val.numpy(), allow_pickle=False)
+	#torch.save(d[key], 'dmcontrolnet/' + key + '.pt')
+
+dmcontrolnet = nn.Sequential(
+	nn.Linear(ncentroids, 1024), 
+	nn.LeakyReLU(0.2), 
+	nn.Linear(1024, 512), 
+	nn.LeakyReLU(0.2), 
+	nn.Linear(512, 256), 
+	nn.LeakyReLU(0.2), 
+	nn.Linear(256, 97)).cuda(device)
+
+dmcontrolnet.load_state_dict(d)
+# dmcontrolnet.load_state_dict(analyzer.state_dict())
+
+# check this simplified network is working.
+sumloss = 0.0
+with torch.no_grad():
+	for k in range(0, nvalidate, batch_size):
+		r = torch.arange(k, k+batch_size, 1, dtype=torch.int64)
+		x = A[r, :].cuda(device) # copy to gpu
+		y = v[r, :].cuda(device)
+		y = y / 0.15 # scale to +- 1.0
+		dmcontrolnet.zero_grad()
+		predict = dmcontrolnet(x)
+		l = lossfunc(y, predict)
+		#print(predict)
+		sumloss = sumloss + l
+
+sumloss = sumloss / (nvalidate / batch_size)
+print(f'validation loss: {sumloss}')
+#torch.save(dmcontrolnet.state_dict(), 'dmControlNet.pt')
+
+#dmcontrolnet.load_state_dict(torch.load('dmControlNet.pt'))
