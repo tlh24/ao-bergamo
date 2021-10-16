fd287c7e1143dcdcde816933bb60e98c55fb0542
Tim Hanson <sideskate@gmail.com>
2021-10-16 17:43:43 -0400

    working optimizergit status

--- matlab/geneopt_si_reset.m
+++ matlab/geneopt_si_reset.m
@@ -7,7 +7,7 @@ genop.udps = dsp.UDPSender('RemoteIPPort', 13131, ...
 
 N = 10e3; 
 genop.N = N;   
-genop.bleach_correct = 70000; 
+genop.bleach_correct = 30000; 
 
 genop.save_dmcommand = single(zeros(N, 97));
 genop.save_vd = single(zeros(N, 1)); 
@@ -18,9 +18,9 @@ genop.DMcommandHist = repmat(genop.DMcommand, 1, 100);
 genop.DMcommandVd = zeros(1, 100);
 genop.DMcommandK = zeros(1, 100);
 
-genop.starttemp = 0.84; % start from zero DM command: 0.005
-genop.endtemp = 0.84; 
+genop.starttemp = 0.05; % start from zero DM command: 0.005
+genop.endtemp = 0.05; 
 genop.temperatures = linspace(genop.starttemp, genop.endtemp, N); 
-genop.k = 1; 
+genop.k = 0; 
 genop.vs = 0;
 tic; 
