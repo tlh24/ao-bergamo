18b44e8f57dd404ebfdb4fe411b42d88b2a2ab6c
Tim Hanson <sideskate@gmail.com>
2021-10-22 15:53:58 -0400

    these are working optimization routines.

--- matlab/geneopt_scanimage.m
+++ matlab/geneopt_scanimage.m
@@ -8,31 +8,45 @@ channels = lastStripe.roiData{1}.channels; % get the channel numbers stored in s
 for idx = 1:length(channels)  
     frame{idx} = lastStripe.roiData{1}.imageData{idx}{1}; % extract all channels
     if idx == 1  
-        frm = frame{idx}; 
-        vd = sum(sum(double(frm) ));   
+        frm = single(frame{idx}); 
+% 		mn = mean(mean(frm)); 
+% 		st = std(std(frm)); 
+% 		genop.avstd = 0.99 * genop.avstd + 0.01 * st; 
+% 		msk = frm > mn + 0.5*genop.avstd; 
+% 		vd = sum(sum((frm .* msk)));   
+		vd = sum(sum(frm .* genop.mask')); 
     end   
 end
 
 k = genop.k; 
 N = genop.N; 
-if mod(k, 2) == 0
-    %genop.vd = vd;
+md = mod(k, genop.averageFrames+1); 
+if md == 0
+    % skip the first frame, it's a transient. 
+	genop.vd = 0; 
     genop.k = k + 1; 
-% elseif mod(k,3) == 1
-%     genop.vd = vd;
-%     genop.k = k + 1; 
-else
-% vd = vd + genop.vd;
+end
+if md > 0 && md < genop.averageFrames
+	genop.vdarr(md) = vd; 
+    genop.k = k + 1; 
+end
+if md == genop.averageFrames
+genop.vdarr(md) = vd; 
+vds = median(genop.vdarr); 
 
 genop.DMcommandHist = [genop.DMcommandHist genop.DMcommand]; 
-genop.DMcommandVd = [genop.DMcommandVd vd]; 
+genop.DMcommandVd = [genop.DMcommandVd vds]; 
 genop.DMcommandK = [genop.DMcommandK k]; 
-if 1
-    if mean(k-genop.DMcommandK) > 2450
+if 0
+    if mean(k-genop.DMcommandK) > 1200
         genop.bleach_correct = genop.bleach_correct * 0.99461; 
+		bleach_correct = genop.bleach_correct; 
+		bleach_correct
     end
     if mean(k-genop.DMcommandK) < 100 && k > 1000
-        genop.bleach_correct = genop.bleach_correct * 1.00137; 
+        genop.bleach_correct = genop.bleach_correct * 1.00137;
+		bleach_correct = genop.bleach_correct; 
+		bleach_correct
     end
 end
 agedecay = 1-((k - genop.DMcommandK) / genop.bleach_correct); 
@@ -41,39 +55,57 @@ agedecay = 1-((k - genop.DMcommandK) / genop.bleach_correct);
 % seems with 7% power at 950nm, it halves over 10k frames. 
 % or goes from ~ 14 to 10 over 5k frames, slope of ~= 
 [~, indx] = sort(genop.DMcommandVd .* agedecay, 'descend'); 
-genop.DMcommandVd = genop.DMcommandVd(indx(1:100)); 
-genop.DMcommandHist = genop.DMcommandHist(:, indx(1:100)); 
-genop.DMcommandK = genop.DMcommandK(:, indx(1:100)); 
+genop.DMcommandVd = genop.DMcommandVd(indx(1:genop.npop)); 
+genop.DMcommandHist = genop.DMcommandHist(:, indx(1:genop.npop)); 
+genop.DMcommandK = genop.DMcommandK(:, indx(1:genop.npop)); 
 
 % now we can pick & select a new DM command.
-temperature = genop.temperatures(mod(k-1,N)+1); 
-pick = floor(rand(1) * 99) + 1; 
+temperature = genop.temperatures(min(k+1, N)); 
+pick = floor(rand(1) * (genop.npop-1)) + 1; 
 father = genop.DMcommandHist(:,pick); 
 pick2 = pick; 
 while pick2 == pick
-    pick2 = floor(rand(1) * 99) + 1; 
+    pick2 = floor(rand(1) * (genop.npop-1)) + 1; 
 end
 mother = genop.DMcommandHist(:,pick2); 
 recomb = randn(97, 1); 
+% recomb = zeros(97, 1); % removing recombination strongly affects convergence. 
 kid = father .* (recomb > 0) + mother .* (recomb < 0); 
-noise = (randn(97,1)*temperature) .* (rand(97, 1) > 0.83);
-noise = noise .* [ones(30, 1); zeros(67, 1)]; 
-% noise = zeros(97, 1); 
-% tax = mod(floor(k/400), 40)+1; 
-% noise(tax) = randn(1)*temperature; 
+noise = (randn(97,1)*temperature) .* (rand(97, 1) > 0.8);
+noise = noise .* [zeros(3,1); ones(27, 1); zeros(67, 1)]; 
+
+noise = zeros(97, 1); 
+tax = mod(floor(k/200), 27)+3; 
+tax = floor(rand(1) * 82)+4;  
+noise(tax) = randn(1)*temperature; 
 genop.DMcommand = reshape(kid+noise, 97, 1); 
 
 % tcmd = zeros(97, 1); 
 % tax = floor(k/300)+1
 % tcmd(tax) = 4*sin(k/10);
-% genop.udps(single([3.1415926; tcmd])); 
-genop.udps(single([3.1415926; genop.DMcommand]));
+% genop.udps(single([3.1415926; zeros(97,1)])); 
+% genop.udps(single([3.1415926; DMcommand_save])); 
+genop.udps(single([3.1415926; genop.DMcommand + genop.hidden]));
 
-genop.save_dmcommand(k, :) = genop.DMcommand;
-genop.save_vd(k) = vd;
-genop.save_time(k) = toc; 
-
-disp([genop.DMcommandVd(1) mean(genop.DMcommandVd) temperature*1e6 vd]); % display the best one. 
-
+s = sprintf('%1.5e %1.5e %1.5e %1.5e %1.5e', ...
+	genop.DMcommandVd(1), mean(genop.DMcommandVd), temperature, vd, tax); % display the best one. 
+disp(s); 
 genop.k = k + 1; 
-end % else
+end % if mod
+
+if k > 0
+	genop.save_dmcommand(k, :) = genop.DMcommand;
+	genop.save_vd(k) = vd;
+	genop.save_time(k) = toc; 
+end
+
+if mod(k,700) == 699
+	% nuke it b/c of bleaching..
+	genop.DMcommandVd = zeros(1, genop.npop);
+end
+% note: 
+% if everything is behaving well (disconneted from internet, e.g), then
+% running SI at 73hz / 300 x 512 resolution seems to mostly reliably give
+% the right intensity reading after two clocks.  
+% this means that in the test above, you'll still get on average 4 clocks
+% of reliable data out of the 5 in the 50% duty cycle.  
--- matlab/geneopt_si_reset.m
+++ matlab/geneopt_si_reset.m
@@ -5,22 +5,40 @@ global genop;
 genop.udps = dsp.UDPSender('RemoteIPPort', 13131, ...
     'RemoteIPAddress','10.123.1.152'); 
 
-N = 10e3; 
+N = 4e4; 
 genop.N = N;   
 genop.bleach_correct = 30000; 
+genop.npop = 25; 
 
 genop.save_dmcommand = single(zeros(N, 97));
 genop.save_vd = single(zeros(N, 1)); 
 genop.save_time = single(zeros(N, 1)); 
 
 genop.DMcommand = zeros(97, 1);
-genop.DMcommandHist = repmat(genop.DMcommand, 1, 100); 
-genop.DMcommandVd = zeros(1, 100);
-genop.DMcommandK = zeros(1, 100);
-
-genop.starttemp = 0.05; % start from zero DM command: 0.005
-genop.endtemp = 0.05; 
+genop.DMcommandHist = repmat(genop.DMcommand, 1, genop.npop); 
+genop.DMcommandVd = zeros(1, genop.npop);
+genop.DMcommandK = zeros(1, genop.npop);
+if 0
+	genop.DMcommand = DMcommand_save; 
+	genop.DMcommandHist = DMcommand_Hist; 
+end
+genop.starttemp = 0.37; % start from zero DM command: 0.005
+genop.endtemp = 0.07; 
 genop.temperatures = linspace(genop.starttemp, genop.endtemp, N); 
 genop.k = 0; 
 genop.vs = 0;
+genop.averageFrames = 9; 
+genop.vdarr = zeros(genop.averageFrames, 1);
+genop.avstd = 0.0; 
+genop.hidden = [randn(20, 1); zeros(77, 1)] * 0.65; 
+genop.hidden = zeros(97, 1); 
+
+genop.mask = zeros(300, 512); 
+for r = 1:300
+	for c = 1:512
+		if abs(r-150) < 50 && abs(c-256) < 60
+			genop.mask(r,c) = 1; 
+		end
+	end
+end
 tic; 
